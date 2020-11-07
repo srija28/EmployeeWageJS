0d3bff3a9c52c60f41bbf4b7b519da4fca285b27
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-11-07 21:17:05 +0530

    [Shreya]Added:Calculate wage till working hours of 160 or max days of 20 is reached in a month

--- EmpWage.js
+++ EmpWage.js
@@ -3,7 +3,8 @@ const IS_PART_TIME = 1;
     const PART_TIME_HOURS = 4;
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOUR = 20;
-    const NUM_OF_WORKING_DAYS =20;
+    const NUM_OF_WORKING_DAYS =10;
+    const MAX_HRS_IN_A_MONTH =100;
     function getWorkingHours(empCheck){
     switch(empCheck){
         case IS_PART_TIME:
@@ -14,12 +15,13 @@ const IS_PART_TIME = 1;
             return 0;    
     } 
     }
-    
-    let empHrs = 0;
-    for(let day =0;day<NUM_OF_WORKING_DAYS;day++){
+    let totalEmpHrs =0;
+    let totalWorkingDays =0;
+    while(totalEmpHrs<= MAX_HRS_IN_A_MONTH && totalWorkingDays< NUM_OF_WORKING_DAYS){
+        totalWorkingDays++;
         let empCheck = Math.floor(Math.random()*10)%3;
-        empHrs += getWorkingHours(empCheck);
+        totalEmpHrs += getWorkingHours(empCheck);
     }
-    let empWage = empHrs * WAGE_PER_HOUR;
-    console.log("Employee total working hours in a Month : "+empHrs);
+    let empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log("Employee total working hours in a Month : "+totalEmpHrs);
     console.log("Employee Wage for a Month is : "+empWage);
