7f5232f71b2e9e6b449594c641481de64462e100
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-11-07 21:14:17 +0530

    [Shreya]Added:Calculate wage for a month

--- EmpWage.js
+++ EmpWage.js
@@ -1,22 +1,25 @@
 const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-
-function getWorkingHours(empCheck){
-switch(empCheck){
-    case IS_PART_TIME:
-        return PART_TIME_HOURS;
-    case IS_FULL_TIME:
-        return FULL_TIME_HOURS;    
-    default:
-        return 0;    
-} 
-}
-
-let empCheck = Math.floor(Math.random()*10)%3;
-let empHrs = getWorkingHours(empCheck);
-let empWage = empHrs*WAGE_PER_HOUR;
-console.log("Working hours of the employee are: "+empHrs+" hrs");
-console.log("Employee wage:" +empWage+" $");
+    const IS_FULL_TIME = 2;
+    const PART_TIME_HOURS = 4;
+    const FULL_TIME_HOURS = 8;
+    const WAGE_PER_HOUR = 20;
+    const NUM_OF_WORKING_DAYS =20;
+    function getWorkingHours(empCheck){
+    switch(empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;    
+        default:
+            return 0;    
+    } 
+    }
+    
+    let empHrs = 0;
+    for(let day =0;day<NUM_OF_WORKING_DAYS;day++){
+        let empCheck = Math.floor(Math.random()*10)%3;
+        empHrs += getWorkingHours(empCheck);
+    }
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("Employee total working hours in a Month : "+empHrs);
+    console.log("Employee Wage for a Month is : "+empWage);
