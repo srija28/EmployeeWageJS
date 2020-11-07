af1dbdb14e94851531c27fc82e565550f56aee9e
shreya <shreyareddykethireddy@shreyas-MacBook-Air.local>
2020-11-07 21:11:17 +0530

    [Shreya]Added: function for calculating daily wage

--- EmpWage.js
+++ EmpWage.js
@@ -1,20 +1,22 @@
-const IS_PART_TIME=1;
+const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
-let empCheck = Math.floor(Math.random()* 10)%3;
+function getWorkingHours(empCheck){
 switch(empCheck){
     case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
+        return PART_TIME_HOURS;
     case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
+        return FULL_TIME_HOURS;    
     default:
-        empHrs=0;
+        return 0;    
+} 
 }
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("UC2 - Emp Wage:"+ empWage);
+
+let empCheck = Math.floor(Math.random()*10)%3;
+let empHrs = getWorkingHours(empCheck);
+let empWage = empHrs*WAGE_PER_HOUR;
+console.log("Working hours of the employee are: "+empHrs+" hrs");
+console.log("Employee wage:" +empWage+" $");
